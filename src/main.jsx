6d5f11bd13eb6628f53be5317e6e5cc93bdae9e2
miraj206 <mdmiraj30ali@gmail.com>
2025-12-12 22:50:57 +0530

    chore: add version log for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {console.log('App Version: SuperMoney-v2 (Checking Deployment)')}
     <App />
   </StrictMode>,
 )
